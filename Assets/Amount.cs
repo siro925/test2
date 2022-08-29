cec64e5e5e17b57963b80c499996f1b5e1fd7e2c
siro <shellinukun@outlook.jp>
2022-08-29 13:44:26 +0900

    Create Amount.cs

---
+++ Assets/Amount.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Amount : MonoBehaviour
+{
+    public Items_Info itemData;
+    public TextMeshProUGUI amountText;
+
+    public int count = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        amountText.text = count.ToString();
+    }
+}
